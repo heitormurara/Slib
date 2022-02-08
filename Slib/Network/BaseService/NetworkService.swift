7f61ed6314118aa4f2ce6aceddd668f704e4bfe9
Heitor Murara <heitor.murara@avenuecode.com>
2022-02-08 20:58:54 -0300

    network service

--- Slib/Network/BaseService/NetworkService.swift
+++ Slib/Network/BaseService/NetworkService.swift
@@ -2,10 +2,25 @@ import Foundation
 
 final class NetworkService<Route: Endpoint>: NetworkServiceProtocol {
     
+    private var urlSessionTask: URLSessionTask?
+    
     func request(_ route: Route,
                  completion: @escaping NetworkServiceCompletion) {
         let session = URLSession.shared
         
+        do {
+            let request = try route.asURLRequest()
+            urlSessionTask = session.dataTask(with: request) { data, response, error in
+                completion(data,
+                           response,
+                           error)
+            }
+        } catch {
+            completion(nil,
+                       nil,
+                       error)
+        }
         
+        urlSessionTask?.resume()
     }
 }
